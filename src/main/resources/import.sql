78f104eb0450c647924cebdb7ee7915476acb45d
Bruno Camargo <brunovieiracamargo@gmail.com>
2023-05-17 15:56:00 -0300

    feat: add development profiles.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,17 +1,3 @@
--- This file allow to write SQL commands that will be emitted in test and dev.
--- The commands are commented as their support depends of the database
--- insert into myentity (id, field) values(2, 'field-2');
--- insert into myentity (id, field) values(3, 'field-3');
--- alter sequence myentity_seq restart with 4;
-
--- CREATE TABLE tb_game(
---     id SERIAL PRIMARY KEY,
---     title VARCHAR(255),
---     description VARCHAR(255),
---     release_date DATE,
---     trailer_url VARCHAR(255)
--- );
-
 insert into tb_game (
     title,
     description,
